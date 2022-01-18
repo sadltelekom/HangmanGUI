31cfc9533b39e03adeb5fab7cd762ff8b806ed18
Sebastian Alaimo Di Loro <sebastian.alaimo.di.loro@telekom.de>
2022-01-18 14:35:50 +0100

    Implemented QuitGameButton.

--- src/main/java/com/example/HangmanGUI/Main.java
+++ src/main/java/com/example/HangmanGUI/Main.java
@@ -1,8 +1,11 @@
 package com.example.HangmanGUI;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
@@ -21,11 +24,24 @@ public class Main extends Application {
         Label introLabel = new Label();
         introLabel.setTextAlignment(TextAlignment.CENTER);
         introLabel.setText("Hello fellow User.\nWelcome to our (Bojan, Szymon and Sebastian) Hangman Game.\n7 tries will determine if you live or die ... \nSo choose wise.");
-        box.getChildren().addAll(introLabel);
+
         box.setSpacing(10);
         box.setPadding(new Insets(10, 10, 10, 10));
+        // Add Buttons to Box
+        Button playGameButton = new Button();
+        playGameButton.setText("Play Game!");
 
+        Button exitGameButton = new Button("Quit Game!");
+        box.getChildren().addAll(introLabel,playGameButton,exitGameButton);
 
+        // Add Event Handlers to buttons:
+        exitGameButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.exit(0);
+            }
+        });
 
         // setting up a scene with that layout
         Scene firstScene = new Scene(box,640,480);
